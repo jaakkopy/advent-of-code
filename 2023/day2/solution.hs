824b95514edebc25ef6be46bf68eed489fbe9a1f
Jaakko <jaakkopyrhonen@gmail.com>
2023-12-02 11:29:12 +0200

    day 2 part 1

---
+++ 2023/day2/solution.hs
@@ -0,0 +1,49 @@
+import Data.Char (isDigit, isSpace)
+import Data.List (isPrefixOf)
+
+limits = [12, 13, 14] -- Red = 0, Green = 1, Blue = 2
+
+parseCube :: String -> [Int]
+parseCube x = cubeList 
+    where
+        digitText = takeWhile (isDigit) (dropWhile isSpace x)
+        amountCubes = read digitText :: Int
+        colorText = dropWhile isSpace $ drop (length digitText + 1) x
+        cubeList = 
+            if isPrefixOf "red" colorText then [amountCubes, 0, 0]
+            else if isPrefixOf "green" colorText then [0, amountCubes, 0]
+            else [0, 0, amountCubes]
+
+
+parseRound :: String -> [Int]
+parseRound x
+    | null x = [0, 0, 0]
+    | otherwise = zipWith (+) cubes (parseRound (drop (length cubeText + 1) x))
+    where 
+        cubeText = takeWhile (/=',') x
+        cubes = parseCube cubeText
+
+parseGame :: String -> [[Int]]
+parseGame x = parseGame' (drop 2 (dropWhile (/=':') x))
+    where parseGame' xs
+            | null xs = []
+            | otherwise = (parseRound round):parseGame' (drop (length round + 1) xs)
+            where round = takeWhile (/=';') xs
+
+readGames :: [String] -> [[[Int]]] -- games, game, rounds (list of lists of lists)
+readGames (x:xs)
+    | null xs = [parseGame x]
+    | otherwise = (parseGame x):(readGames xs)
+
+countPossible :: [[[Int]]] -> Int -> Int
+countPossible xs i
+    | null xs = 0
+    | otherwise = (if all (==True) [verify x | x <- head xs] then i else 0) + countPossible (tail xs) (i+1)
+    where 
+        verify round = all (==True) $ zipWith (<=) round limits
+
+main = do
+    contents <- getContents
+    let ls = lines contents
+        games = readGames ls
+    print $ countPossible games 1
