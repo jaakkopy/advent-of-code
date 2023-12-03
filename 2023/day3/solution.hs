6427bbad0b2f35dbf7c750806e83d0122482ceb2
Jaakko <jaakkopyrhonen@gmail.com>
2023-12-03 14:21:53 +0200

    day 3 part 2

--- 2023/day3/solution.hs
+++ 2023/day3/solution.hs
@@ -16,24 +16,36 @@ isSpecialSymbol m r c ri ci
     | inBounds r c ri ci = (((m !! ri) !! ci) /= '.') && ((not . isDigit) ((m !! ri) !! ci))
     | otherwise = False
 
-checkAdjacent :: [[Char]] -> Int -> Int -> Int -> Int -> Bool
-checkAdjacent m r c ri ci = if (inBounds r c ri ci) && (isDigit ((m !! ri) !! ci)) 
-    then if foldl (\acc (y,x) -> acc || (isSpecialSymbol m r c (ri+y) (ci+x))) False directions 
-        then Just(extractNumStr (m !! ri) ri ci)
-        else Nothing
-    else Nothing
 
-findSymbolAdjacentNums :: [[Char]] -> Int -> Int -> Int -> Int -> [Int]
+checkDirection m r c endc ri ci
+    | ci >= endc = []
+    | otherwise = case (if (inBounds r c ri ci) && (isDigit (m!!ri!!ci)) then Just(extractNumStr (m!!ri) ri ci) else Nothing) of 
+        Just (left, right) -> (left ++ right):(checkDirection m r c endc ri (ci + (length right)))
+        Nothing            -> checkDirection m r c endc ri (ci+1)
+
+
+getAdjacentNums m r c ri ci = numsUp ++ numsDown ++ numsLeft ++ numsRight
+    where
+        numsUp    = checkDirection m r c (ci+2) (ri-1) (ci-1) 
+        numsDown  = checkDirection m r c (ci+2) (ri+1) (ci-1) 
+        numsLeft  = checkDirection m r c ci ri (ci-1) 
+        numsRight = checkDirection m r c (ci+2) ri (ci+1) 
+
 findSymbolAdjacentNums m r c ri ci
     | ri == r = []
-    | ci >= c = findSymbolAdjacentNums m r c (ri+1) 0
-    | otherwise = case (checkAdjacent m r c ri ci) of 
-        Just (left, right) -> (read (left ++ right) :: Int):(findSymbolAdjacentNums m r c ri (ci + (length right)))
-        Nothing -> findSymbolAdjacentNums m r c ri (ci + 1)
+    | ci == c = findSymbolAdjacentNums m r c (ri+1) 0
+    | ((not . isDigit) sym) && (sym /= '.') = [[sym]:(getAdjacentNums m r c ri ci)] ++ (findSymbolAdjacentNums m r c ri (ci + 1))
+    | otherwise = findSymbolAdjacentNums m r c ri (ci+1)
+    where
+        sym = (m !! ri) !! ci
 
 main = do
     contents <- getContents
     let ls = lines contents
         nrow = length ls
         ncol = length (head ls)
-    print $ sum $ findSymbolAdjacentNums ls nrow ncol 0 0
+        nums = findSymbolAdjacentNums ls nrow ncol 0 0
+        sumOfInts = sum $ map (\xs -> foldl (+) 0 [read x :: Int | x <- xs]) $ map tail nums
+        gearRatios = sum $ map (\xs -> foldl (*) 1 [read x :: Int | x <- xs]) $ [tail xs | xs <- nums, head xs == "*" && length xs == 3]
+    print sumOfInts -- part 1
+    print gearRatios -- part 2
