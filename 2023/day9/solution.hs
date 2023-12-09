aac58c12bb95a69b4a0885596389c54464f77727
Jaakko <jaakkopyrhonen@gmail.com>
2023-12-09 19:55:44 +0200

    day 9 part 2

--- 2023/day9/solution.hs
+++ 2023/day9/solution.hs
@@ -1,17 +1,17 @@
 
-
 makeSequences :: [Int] -> [[Int]]
 makeSequences history
     | all (==0) history = [history]
     | otherwise = history : makeSequences next
     where next = zipWith (-) (tail history) history
 
-extrapolate :: [[Int]] -> Int
-extrapolate (s:sequences)
+extrapolateWith :: Num a => (b -> c) -> (c -> a -> a) -> [b] -> a
+extrapolateWith pickFrom combineWith (s:sequences)
     | null sequences = 0
-    | otherwise = (last s) + (extrapolate sequences)
+    | otherwise = (pickFrom s) `combineWith` (extrapolateWith pickFrom combineWith sequences)
 
 main = do
     contents <- getContents
     let histories = map (\x -> map (\y -> read y :: Int) x) $ map words $ lines contents
-    print $ sum $ map (extrapolate . makeSequences) histories
+    print $ sum $ map ( (extrapolateWith last (+)) . makeSequences) histories -- part 1
+    print $ sum $ map ( (extrapolateWith head (-)) . makeSequences) histories -- part 2
