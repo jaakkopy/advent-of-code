80a1319e89b94122a5f7b32acfa112c68a7a8ba5
Jaakko <jaakkopyrhonen@gmail.com>
2023-12-12 17:14:47 +0200

    day 11 part 2

--- 2023/day11/solution.py
+++ 2023/day11/solution.py
@@ -1,4 +1,5 @@
-def create_adjacency_matrix(M):
+def create_adjacency_matrix(M, distance_coefficient):
+    distance_coefficient = 1 if distance_coefficient == 1 else distance_coefficient - 1
     rows = len(M)
     cols = len(M[0])
     nodes = []
@@ -19,26 +20,24 @@ def create_adjacency_matrix(M):
                 continue
             D[i][j] = abs(nodes[i][0] - nodes[j][0]) + abs(nodes[i][1] - nodes[j][1]) 
             # check if there is a no-galaxy column between the galaxies:
-            startc = min(nodes[i][1], nodes[j][1])
-            endc =  max(nodes[i][1], nodes[j][1]) + 1
-            for c in range(startc, endc, 1):
+            for c in range(min(nodes[i][1], nodes[j][1]), max(nodes[i][1], nodes[j][1]) + 1, 1):
                 if no_galaxy_cols[c]:
-                    D[i][j] += 1
+                    D[i][j] += distance_coefficient
             # check if there is a no-galaxy row between the galaxies:
-            startr = min(nodes[i][0], nodes[j][0])
-            endr =  max(nodes[i][0], nodes[j][0]) + 1
-            for r in range(startr, endr, 1):
+            for r in range(min(nodes[i][0], nodes[j][0]), max(nodes[i][0], nodes[j][0]) + 1, 1):
                 if no_galaxy_rows[r]:
-                    D[i][j] += 1
+                    D[i][j] += distance_coefficient
     return D 
 
-
-if __name__ == "__main__":
-    from sys import stdin
-    M = [x.rstrip() for x in stdin.readlines()]
-    D = create_adjacency_matrix(M)
+def sum_distances(D):
     s = 0
     for v in range(len(D)):
         for u in range(v, len(D)):
             s += D[v][u]
-    print(s) # part 1
+    return s
+
+if __name__ == "__main__":
+    from sys import stdin
+    M = [x.rstrip() for x in stdin.readlines()]
+    print(sum_distances(create_adjacency_matrix(M, 1))) # part 1
+    print(sum_distances(create_adjacency_matrix(M, 1000000))) # part 2
