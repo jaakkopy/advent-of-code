b5d36f9390b96df0ce1664fdcdb6339efcb1d945
Jaakko <jaakkopyrhonen@gmail.com>
2023-12-12 15:53:22 +0200

    day 10 part 2

--- 2023/day10/solution.py
+++ 2023/day10/solution.py
@@ -45,7 +45,7 @@ def get_allowed_positions(v, pipe_type, r, c):
     if pipe_type & 0b1000 and v[0] - 1 >= 0:
         yield (v[0] - 1, v[1])
 
-def bfs(G, start):
+def find_farthest_away(G, start):
     rows = len(G)
     cols = len(G[0])
     visited = set([start])
@@ -60,9 +60,43 @@ def bfs(G, start):
                 Q.append((pos, dist + 1))
     return max_dist
 
+def find_loop(G, start):
+    rows = len(G)
+    cols = len(G[0])
+    step = next(get_allowed_positions(start, G[start[0]][start[1]], rows, cols))
+    visited = set([start, step]) # Begin the cycle by taking a step in any path from the starting position
+    S = [step]
+    path = [start]
+    while len(S) > 0:
+        v = S.pop()
+        path.append(v)
+        for pos in get_allowed_positions(v, G[v[0]][v[1]], rows, cols):
+            if pos not in visited:
+                visited.add(pos)
+                S.append(pos)
+    return path
+
+def find_area_inside_loop(G, start):
+    '''
+    A = 1/2 * |sum( (y_i+1 + y_i)*(x_i - x_i+1))| where the points are in counter clockwise or clockwise order: https://en.wikipedia.org/wiki/Shoelace_formula
+    A = i + b/2 - 1 for integer coordinates where i = interior points and b = points on the border: https://en.wikipedia.org/wiki/Pick%27s_theorem
+    
+    solve for i:
+    i + b/2 - 1 = 1/2 * sum( (y_i+1 + y_i)*(x_i - x_i+1))  
+    i = 1/2 * sum( (y_i+1 + y_i)*(x_i - x_i+1)) - b/2 + 1
+    '''
+    loop = find_loop(G, start)
+    s = 0
+    for i in range(0, len(loop)-1):
+        s += (loop[i+1][0] + loop[i][0])*(loop[i][1] - loop[i+1][1]) 
+    s  = abs(s/2)
+    return s - len(loop)/2 + 1
 
 if __name__ == "__main__":
     from sys import stdin
     lines = [x.rstrip() for x in stdin.readlines()]
     G, start = make_graph(lines)
-    print(bfs(G, start))
+    max_dist = find_farthest_away(G, start)
+    print(max_dist) # Part 1
+    a = find_area_inside_loop(G, start)
+    print(a)
