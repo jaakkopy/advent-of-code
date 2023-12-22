c17d7504c9cf449259f8c3539f77989a35177277
Jaakko <jaakkopyrhonen@gmail.com>
2023-12-22 23:07:14 +0200

    day 16 part 1

---
+++ 2023/day16/solution.py
@@ -0,0 +1,90 @@
+def in_bounds(rows, cols, r, c):
+    return (r >= 0 and r < rows) and (c >= 0 and c < cols)
+
+
+def up(pos_and_dir, p, d):
+    pos_and_dir.append(((p[0] - 1, p[1]), (-1, 0)))
+
+
+def down(pos_and_dir, p, d):
+    pos_and_dir.append(((p[0] + 1, p[1]), (1, 0)))
+
+
+def left(pos_and_dir, p, d):
+    pos_and_dir.append(((p[0], p[1] - 1), (0, -1)))
+
+
+def right(pos_and_dir, p, d):
+    pos_and_dir.append(((p[0], p[1] + 1), (0, 1)))
+
+
+def vertical_pipe(pos_and_dir, p, d):
+    # |
+    up(pos_and_dir, p, d)
+    down(pos_and_dir, p, d)
+
+
+def horizontal_pipe(pos_and_dir, p, d):
+    # -
+    left(pos_and_dir, p, d)
+    right(pos_and_dir, p, d)
+
+
+def mirror1(pos_and_dir, p, d):
+    # \
+    if d[1] == 1:
+        down(pos_and_dir, p, d)
+    elif d[1] == -1:
+        up(pos_and_dir, p, d)
+    elif d[0] == 1:
+        right(pos_and_dir, p, d)
+    elif d[0] == -1:
+        left(pos_and_dir, p, d)
+
+
+def mirror2(pos_and_dir, p, d):
+    # /
+    if d[1] == 1:
+        up(pos_and_dir, p, d)
+    elif d[1] == -1:
+        down(pos_and_dir, p, d)
+    elif d[0] == 1:
+        left(pos_and_dir, p, d)
+    elif d[0] == -1:
+        right(pos_and_dir, p, d)
+
+
+def follow_light_paths(lines: list[str]):
+    rows = len(lines)
+    cols = len(lines[0])
+    visited = set()
+    pos_and_dir = [((0, 0), (0, 1))]
+    while len(pos_and_dir) > 0:
+        pd = pos_and_dir.pop()
+        (p, d) = pd
+        (r, c) = p
+        if pd in visited or not in_bounds(rows, cols, r, c):
+            continue
+        visited.add(pd)
+        if lines[r][c] == '|' and d[1] != 0:
+            vertical_pipe(pos_and_dir, p, d)
+        elif lines[r][c] == '-' and d[0] != 0:
+            horizontal_pipe(pos_and_dir, p, d)
+        elif lines[r][c] == '\\':
+            mirror1(pos_and_dir, p, d)
+        elif lines[r][c] == '/':
+            mirror2(pos_and_dir, p, d)
+        else:
+            nextpd = ((p[0] + d[0], p[1] + d[1]), d)
+            if in_bounds(rows, cols, nextpd[0][0], nextpd[0][1]):
+                pos_and_dir.append(nextpd)
+    return visited
+
+
+if __name__ == "__main__":
+    from sys import stdin
+    lines = [list(x.rstrip()) for x in stdin.readlines()]
+    markers = [x.copy() for x in lines]
+    visited = follow_light_paths(lines)
+    unique_positions = len(set([x[0] for x in visited]))
+    print(unique_positions)
