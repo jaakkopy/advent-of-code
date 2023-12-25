e8cd6f73f493e6f2c86b38acbbc7a21266bce3b3
Jaakko <jaakkopyrhonen@gmail.com>
2023-12-25 18:38:58 +0200

    updated day 18 part 1

--- 2023/day18/solution.py
+++ 2023/day18/solution.py
@@ -4,19 +4,21 @@ def gen_points(guide):
         p = points[-1]
         d = int(g[1])
         match g[0]:
-            case 'R': points.extend([(p[0], p[1] + x) for x in range(1, d+1)])
-            case 'D': points.extend([(p[0] + y, p[1]) for y in range(1, d+1)])
-            case 'L': points.extend([(p[0], p[1] - x) for x in range(1, d+1)])
-            case 'U': points.extend([(p[0] - y, p[1]) for y in range(1, d+1)])
+            case 'R': points.append((p[0], p[1] + d))
+            case 'D': points.append((p[0] + d, p[1]))
+            case 'L': points.append((p[0], p[1] - d))
+            case 'U': points.append((p[0] - d, p[1]))
     return points
 
 
 def calculate_area(points):
     s = 0
+    b = 0
     for i in range(1, len(points)):
+        b += abs(points[i-1][0] - points[i][0]) + abs(points[i-1][1] - points[i][1])
         s += (points[i][0] + points[i-1][0]) * (points[i-1][1] - points[i][1])
-    i = 0.5 * abs(s) + 1 - len(points)/2
-    return int(i + len(points))
+    i = 0.5 * abs(s) + 1 - b/2
+    return int(i + b)
 
 
 if __name__ == "__main__":
